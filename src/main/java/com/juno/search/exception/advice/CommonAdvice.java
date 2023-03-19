b0dfab905e2d66ed307bf5e1cf1642042b4e6caf
juno-choi <ililil9482@naver.com>
2023-03-19 09:00:04 +0900

    [기능] 필수 parameter MissingServletRequestParameterException 처리 추가

--- src/main/java/com/juno/search/exception/advice/CommonAdvice.java
+++ src/main/java/com/juno/search/exception/advice/CommonAdvice.java
@@ -4,19 +4,40 @@ import com.juno.search.domain.vo.api.ApiError;
 import com.juno.search.domain.vo.api.ErrorVo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.juno.search")
 public class CommonAdvice {
     @ExceptionHandler
     public ResponseEntity<ApiError> illegalArgumentException(IllegalArgumentException e){
         List<ErrorVo> errors = new ArrayList<>();
         errors.add(ErrorVo.builder()
                 .message(e.getMessage())
+                .build());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ApiError.builder()
+                        .code("FAIL")
+                        .message("실패")
+                        .errors(errors)
+                        .build()
+        );
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ApiError> missingServletRequestParameterException(MissingServletRequestParameterException e){
+        StringBuilder sb = new StringBuilder();
+        sb.append("필수 parameter = ");
+        sb.append(e.getParameterName());
+        sb.append("("+e.getParameterType()+")");
+
+        List<ErrorVo> errors = new ArrayList<>();
+        errors.add(ErrorVo.builder()
+                .message(sb.toString())
                 .build());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
                 ApiError.builder()
