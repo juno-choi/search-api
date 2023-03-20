42987bb6a69fc3828d98bab019ed056633db013a
juno-choi <ililil9482@naver.com>
2023-03-20 22:13:44 +0900

    [기타] 사용하지 않는 테스트 주석처리

--- src/test/java/com/juno/search/SearchApplicationTests.java
+++ src/test/java/com/juno/search/SearchApplicationTests.java
@@ -3,10 +3,10 @@ package com.juno.search;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SearchApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
