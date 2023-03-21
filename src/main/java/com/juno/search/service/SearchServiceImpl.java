02c66017be52350143a71f81047d7a491e98c460
juno-choi <ililil9482@naver.com>
2023-03-21 15:27:59 +0900

    [기능] /v1/search 호출시 해당 검색 내용을 5분간 캐싱하는 로직 추가 및 단위 테스트 추가

--- src/main/java/com/juno/search/service/SearchServiceImpl.java
+++ src/main/java/com/juno/search/service/SearchServiceImpl.java
@@ -1,5 +1,7 @@
 package com.juno.search.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.juno.search.config.SearchClient;
 import com.juno.search.domain.dto.SearchDto;
 import com.juno.search.domain.entity.Search;
@@ -9,6 +11,7 @@ import com.juno.search.domain.vo.TopSearchVo;
 import com.juno.search.repository.SearchRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +19,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -24,17 +28,67 @@ import java.util.Optional;
 public class SearchServiceImpl implements SearchService{
     private final SearchClient searchClient;
     private final SearchRepository searchRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper objectMapper;
 
     @Override
     @Transactional
     public SearchListVo search(SearchDto search) {
         String keyword = search.getQuery().trim().toLowerCase(Locale.ROOT);
+        String hashKey = getHashKey(search);
+        SearchListVo searchListVo;
 
         Optional<Search> findSearch = searchRepository.findByKeyword(keyword);
         saveSearch(keyword, findSearch);
 
-        SearchListVo searchListVo = searchClient.search(search);
+        Optional<Object> optional = Optional.ofNullable(redisTemplate.opsForHash().get(keyword, hashKey));
+        if(optional.isPresent()){
+            return getSearchListVo(optional);
+        }
+
+        searchListVo = searchClient.search(search);
+        saveRedis(keyword, hashKey, searchListVo);
         return searchListVo;
+    }
+
+    private void saveRedis(String keyword, String hashKey, SearchListVo searchListVo) {
+        try {
+            redisTemplate.opsForHash().put(keyword, hashKey, objectMapper.writeValueAsString(searchListVo));
+        } catch (JsonProcessingException e) {
+            log.error("write error", e);
+        }
+        redisTemplate.expire(keyword, 5L, TimeUnit.MINUTES);  // 5분간 캐싱
+    }
+
+    private String getHashKey(SearchDto search) {
+        StringBuilder hashKeyBuilder = new StringBuilder();
+        hashKeyBuilder.append(search.getType());
+        hashKeyBuilder.append("/");
+        hashKeyBuilder.append(search.getSort());
+        hashKeyBuilder.append("/");
+        hashKeyBuilder.append(search.getPage());
+        hashKeyBuilder.append("/");
+        hashKeyBuilder.append(search.getSize());
+        return hashKeyBuilder.toString();
+    }
+
+    private SearchListVo getSearchListVo(Optional<Object> optional) {
+        String searchListVoStr = (String) optional.get();
+        SearchListVo searchListVo = null;
+        try {
+            searchListVo = objectMapper.readValue(searchListVoStr, SearchListVo.class);
+        } catch (JsonProcessingException e) {
+            log.error("read error", e);
+        }
+        return searchListVo;
+    }
+
+    private void saveSearch(String keyword, Optional<Search> findSearch) {
+        if(findSearch.isPresent()){
+            findSearch.get().plusCount();
+            return ;
+        }
+        searchRepository.save(Search.of(keyword));
     }
 
     @Override
@@ -54,13 +108,5 @@ public class SearchServiceImpl implements SearchService{
         return TopSearchVo.builder()
                 .list(list)
                 .build();
-    }
-
-    private void saveSearch(String keyword, Optional<Search> findSearch) {
-        if(findSearch.isPresent()){
-            findSearch.get().plusCount();
-            return ;
-        }
-        searchRepository.save(Search.of(keyword));
     }
 }
--- src/test/java/com/juno/search/service/SearchServiceImplUnitTest.java
+++ src/test/java/com/juno/search/service/SearchServiceImplUnitTest.java
@@ -6,6 +6,7 @@ import com.juno.search.domain.dto.SearchDto;
 import com.juno.search.domain.entity.Search;
 import com.juno.search.domain.enums.SearchType;
 import com.juno.search.domain.enums.SortType;
+import com.juno.search.domain.vo.DocumentsVo;
 import com.juno.search.domain.vo.SearchListVo;
 import com.juno.search.domain.vo.SearchVo;
 import com.juno.search.domain.vo.TopSearchVo;
@@ -15,18 +16,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SearchServiceImplUnitTest {
-    private ObjectMapper objectMapper = new ObjectMapper();
-
     @InjectMocks
     private SearchServiceImpl searchService;
 
@@ -34,7 +37,16 @@ class SearchServiceImplUnitTest {
     private SearchRepository searchRepository;
 
     @Mock
+    private HashOperations hashOperations;
+
+    @Mock
     private SearchClient searchClient;
+
+    @Mock
+    private ObjectMapper objectMapper;
+
+    @Mock
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Test
     @DisplayName("검색되지 않는 내용일 경우에도 성공한다. (kakao)")
@@ -46,6 +58,10 @@ class SearchServiceImplUnitTest {
                 .listSize(0)
                 .list(new ArrayList<>())
                 .build());
+        given(redisTemplate.opsForHash()).willReturn(hashOperations);
+        given(hashOperations.get(anyString(), anyString())).willReturn(null);
+        doNothing().when(hashOperations).put(anyString(), any(), any());
+        given(objectMapper.writeValueAsString(any())).willReturn("");
 
         //when
         SearchListVo search = searchService.search(searchDto);
@@ -64,10 +80,51 @@ class SearchServiceImplUnitTest {
                 .listSize(0)
                 .list(new ArrayList<>())
                 .build());
+        given(redisTemplate.opsForHash()).willReturn(hashOperations);
+        given(hashOperations.get(anyString(), anyString())).willReturn(null);
+        doNothing().when(hashOperations).put(anyString(), any(), any());
+        given(objectMapper.writeValueAsString(any())).willReturn("");
         //when
         SearchListVo search = searchService.search(searchDto);
         //then
         assertTrue(search.getList().size() == 0);
+    }
+
+    @Test
+    @DisplayName("한번 검색한 내용은 redis에서 데이터를 가져오는데 성공한다.")
+    void searchSuccess3() throws Exception {
+        //given
+        SearchDto searchDto = SearchDto.of(SortType.A, 1, 10, "캐싱될 문구", SearchType.KAKAO);
+        
+        List<DocumentsVo> documentsVos = new ArrayList<>();
+        for(int i=1; i<=10; i++){
+            documentsVos.add(DocumentsVo.builder()
+                    .title("제목"+i)
+                    .contents("내용"+i)
+                    .blogName("블로그 이름")
+                    .thumbnail("썸네일 주소")
+                    .datetime("20230301")
+                    .url("url")
+                    .build());
+        }
+        SearchListVo searchListVo = SearchListVo.builder()
+                .from("KAKAO")
+                .listSize(10)
+                .list(documentsVos)
+                .build();
+
+        String searchListVoAsString = new ObjectMapper().writeValueAsString(searchListVo);
+
+        given(redisTemplate.opsForHash()).willReturn(hashOperations);
+        given(hashOperations.get(anyString(), anyString())).willReturn(searchListVoAsString);
+        given(objectMapper.readValue(searchListVoAsString, SearchListVo.class)).willReturn(searchListVo);
+
+        //when
+        SearchListVo search = searchService.search(searchDto);
+
+        //then
+        assertNotNull(search);
+        verify(objectMapper, times(1)).readValue(searchListVoAsString, SearchListVo.class);
     }
 
     @Test
