b95de6fc3d379bdc0b1ec715787beef70e568210
juno-choi <ililil9482@naver.com>
2023-03-21 11:32:42 +0900

    [기능] 단위 테스트 추가

--- src/test/java/com/juno/search/service/SearchServiceImplUnitTest.java
+++ src/test/java/com/juno/search/service/SearchServiceImplUnitTest.java
@@ -3,9 +3,12 @@ package com.juno.search.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.juno.search.config.SearchClient;
 import com.juno.search.domain.dto.SearchDto;
+import com.juno.search.domain.entity.Search;
 import com.juno.search.domain.enums.SearchType;
 import com.juno.search.domain.enums.SortType;
 import com.juno.search.domain.vo.SearchListVo;
+import com.juno.search.domain.vo.SearchVo;
+import com.juno.search.domain.vo.TopSearchVo;
 import com.juno.search.repository.SearchRepository;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +17,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -66,4 +70,46 @@ class SearchServiceImplUnitTest {
         assertTrue(search.getList().size() == 0);
     }
 
+    @Test
+    @DisplayName("top10 검색에 성공한다.")
+    void topSearchSuccess1() throws Exception {
+        //given
+        List<Search> list = new ArrayList<>();
+        int count = 10000;
+        for(int i=1; i<=10; i++){
+            list.add(Search.of("키워드"+i, count-i));
+        }
+
+        given(searchRepository.findTop10ByOrderByCountDescModifiedAtDesc()).willReturn(list);
+        
+        //when
+        TopSearchVo topSearchVo = searchService.topSearch();
+        
+        //then
+        assertEquals("키워드1", topSearchVo.getList().get(0).getKeyword());
+    }
+    
+    @Test
+    @DisplayName("top10 랭킹값 검색에 성공한다.")
+    void topSearchSuccess2() throws Exception {
+        //given
+        List<Search> list = new ArrayList<>();
+        int count = 10000;
+        for(int i=1; i<=10; i++){
+            list.add(Search.of("키워드"+i, count-i));
+        }
+
+        given(searchRepository.findTop10ByOrderByCountDescModifiedAtDesc()).willReturn(list);
+
+        //when
+        TopSearchVo topSearchVo = searchService.topSearch();
+
+        //then
+        List<SearchVo> searchVoList = topSearchVo.getList();
+        for(int i=1; i<=10; i++){
+            SearchVo searchVo = searchVoList.get(i-1);
+            assertEquals(i, searchVo.getRank());
+            assertEquals(count-i, searchVo.getCount());
+        }
+    }
 }
